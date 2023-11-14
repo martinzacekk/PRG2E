26949fd5f0a7e1510e8d2128db044b0d2579f992
bohac <honza.boh00@gmail.com>
2023-11-14 08:45:29 +0100

    Spol. hodina - Sort, showcase

--- src/arrays/ArraysBasics.java
+++ src/arrays/ArraysBasics.java
@@ -17,5 +17,17 @@ public class ArraysBasics {
         for (int i = 0; i < dalsiPole.length; i++) {
             System.out.print(dalsiPole[i] + " ");
         }
+
+        int[] switches = {5, 6, 9};
+        int temp = switches[0];
+        switches[0] = switches[2];
+        switches[2] = temp;
+
+        for (int i = 0; i < pole.length - 1; i++) {
+            if (pole[i] > pole[i+1]){
+                System.out.println("Neni serazeno");
+                break;
+            }
+        }
     }
 }
